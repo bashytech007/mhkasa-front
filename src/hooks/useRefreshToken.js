4c5749a7b745574eb729590222968346ec386934
chisomchris <christianezeanyaeche@gmail.com>
2024-03-08 23:42:55 +0100

    fix infinite refresh

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,11 +1,16 @@
-import axios from "../utils/axios";
+// import axios from "../utils/axios"
+
+import axios from "axios";
 
 export const useRefreshToken = () => {
   return async () => {
     try {
-      const response = await axios.get("/refresh", {
-        withCredentials: true,
-      });
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/refresh`,
+        {
+          withCredentials: true,
+        }
+      );
       return response.data.accessToken;
     } catch (error) {
       console.log(error?.response?.data?.message);

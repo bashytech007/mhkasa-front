aa4b314c4540fe6ef14fb8d6d38cdee608b40ac2
chisomchris <christianezeanyaeche@gmail.com>
2024-03-17 21:26:33 +0100

    feat: prevent body scroll when modal is opened

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,8 @@
+const css = `
+body {
+  overflow: hidden;
+}
+`;
 export const Modal = ({ title, children }) => {
   return (
     <div className="fixed left-0 right-0 top-0 bottom-0 bg-[#00000020] z-[100] grid">
@@ -5,6 +10,7 @@ export const Modal = ({ title, children }) => {
         <h2 className="text-app-red font-bold py-2 text-xl">{title}</h2>
         {children}
       </div>
+      <style>{css}</style>
     </div>
   );
 };

ab2426d5f0a5c5580a6683519873365a44846ad7
bashytech007 <bashiraremu474@gmail.com>
2024-03-21 09:34:24 +0100

    added-successagain

--- src/pages/success.jsx
+++ src/pages/success.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 export const Component = () => {
   return (
-    <Wrapper className="py-12">
-      <div className="flex flex-col items-center mx-auto my-14 w-fit">
+    <Wrapper className="py-8">
+      <div className="flex flex-col items-center mx-auto my-10 w-fit">
         <img src={success} alt="" />
         <h2 className="mt-6 text-xl font-bold">Successfully</h2>
         <p>your account has been created</p>
         <Link
           to="/"
-          className="bg-app-black px-[124px] text-white mt-6 font-bold text-xl rounded-full py-2"
+          className="bg-app-black px-[120px] text-white mt-6 font-bold text-xl rounded-full py-2"
           replace
         >
           Okay

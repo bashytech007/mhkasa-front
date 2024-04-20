36eab2e690fcb47b135010d837e6dadcd0688e18
Christian <christianchisom278@gmail.com>
2024-04-20 16:23:07 +0100

    post review

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,97 +1,98 @@
 import { Icon } from "@iconify/react";
 import { Button } from "./ui/Button";
-import { useState, useRef } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/utils/useAuth";
 import * as yup from "yup";
 import { useFormik } from "formik";
-import { categoriesLoader } from "../utils/loaders";
+import axios from "../utils/axios";
+import { Input } from "./Input";
+import { useCanSubmitForm } from "../hooks/utils/useCanSubmitFormik";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
-export const ProductDetail = ({productId}) => {
+export const ProductDetail = ({ productId }) => {
+  const { getUserId } = useAuth();
   const schema = yup.object().shape({
     email: yup.string().email().required(),
-    password: yup
-      .string()
-      .trim()
-      .required()
-      .matches(/(?=.*[A-Z])/, "must contain uppercase")
-      .matches(/^(?=.*[a-z])/, "Must contain lowercase")
-      .min(6, "must be at least 6 characters long")
-      .max(50, "must be at most 50 characters long"),
-    // .matches(/(?=.*[^\w\d\s])/, "must contain special character")
+    name: yup.string().trim().required(),
+    review: yup.string().trim().required(),
   });
-  
-  const {getUserId}=useAuth()
-  const [reviews,setReviews] = useState([
-    {
-      review: "hiue iurhfi urefber fbuib chneroir iohf noeri hioferho hofuie rhfioer jhoif erjhof joerhjfio",
-      reviewer: "John Doe",
-      rating: 3,
+
+  const formik = useFormik({
+    validationSchema: schema,
+    initialValues: {
+      name: "",
+      email: "",
+      review: "",
     },
-  ]);
+    onSubmit: handleSubmitReview,
+  });
+
+  const mutation = useMutation({
+    mutationFn: (values) => {
+      return axios.post(`/add/review/${getUserId()}/${productId}`, values, {
+        headers: { "Content-Type": "application/json" },
+      });
+    },
+  });
+
+  const [reviews, setReviews] = useState([]);
 
   const [rating, setRating] = useState(0);
   const [tab, setTab] = useState("reviews");
-
-  const nameRef = useRef(null);
-  const emailRef = useRef(null);
-  const reviewRef = useRef(null);
-
   const onClick = (rate) => {
     setRating(rate);
   };
 
-  const handleSubmitReview = async (e) => {
-    e.preventDefault()
-    const name = nameRef.current.value;
-    const email = emailRef.current.value;
-    const userReview = reviewRef.current.value;
+  const canSubmit = useCanSubmitForm(formik, rating > 0);
 
-    const userId=getUserId()
-     console.log(userId)
-    
-    const data = {
-      name,
-      email,
-      review: userReview,
-      rating,
-    };
-  
-    const endpoint = `/add/review/${userId}/${productId}`;
-
-  try {
-    const response = await axios.post(endpoint, data);
-    console.log("Review submitted:", response.data);
-
-    setReviews([...reviews, {
-      review: userReview,
-      reviewer: name, 
-      rating,  
-    }]);
-    
-    
-    nameRef.current.value = '';
-    emailRef.current.value = '';
-    reviewRef.current.value = '';
-    setRating(0);
-
-  } catch (error) {
-    console.error("Error submitting review:", error);
+  async function handleSubmitReview(values, { resetForm }) {
+    if (rating < 1) {
+      return alert("Please select rating");
+    }
+    mutation.mutate(
+      { rating, ...values },
+      {
+        onSuccess: () => {
+          resetForm();
+          setRating(0);
+        },
+        onError: () => {
+          toast.error("Review submission attempt failed");
+        },
+      }
+    );
   }
-};
-  
+
+  useEffect(() => {
+    /**
+     * fetch data from end point and set here
+     */
+    setReviews([
+      {
+        review:
+          "hiue iurhfi urefber fbuib chneroir iohf noeri hioferho hofuie rhfioer jhoif erjhof joerhjfio",
+        reviewer: "John Doe",
+        rating: 3,
+      },
+    ]);
+  }, []);
 
   return (
-    <div className="bg-white px-6 py-4 rounded-3xl mb-6" >
+    <div className="bg-white px-6 py-4 rounded-3xl mb-6">
       <div className="grid grid-cols-2 border-b-2 md:grid-cols-4">
         <button
-          className={`-mb-[2px] pb-4 ${tab === "description" ? "border-b-black border-b-2 font-bold" : ""}`}
+          className={`-mb-[2px] pb-4 ${
+            tab === "description" ? "border-b-black border-b-2 font-bold" : ""
+          }`}
           onClick={() => setTab("description")}
         >
           Description
         </button>
         <button
-          className={`-mb-[2px] pb-4 ${tab === "reviews" ? "border-b-black border-b-2 font-bold" : ""}`}
+          className={`-mb-[2px] pb-4 ${
+            tab === "reviews" ? "border-b-black border-b-2 font-bold" : ""
+          }`}
           onClick={() => setTab("reviews")}
         >
           Reviews
@@ -99,8 +100,10 @@ export const ProductDetail = ({productId}) => {
       </div>
       <div className="grid gap-6 pt-5 md:grid-cols-2">
         <div>
-          <p className="font-medium text-4xl">4.5</p>
-          <p>Average Ratings</p>
+          <div className="flex gap-4 items-end">
+            <p className="font-medium text-4xl">4.5</p>
+            <p>Average Ratings</p>
+          </div>
           <ul className="grid gap-2 pt-4 max-h-[480px] overflow-y-auto">
             {reviews.map(({ review, reviewer, rating }, i) => (
               <li key={i}>
@@ -109,41 +112,52 @@ export const ProductDetail = ({productId}) => {
             ))}
           </ul>
         </div>
-         {
-          getUserId() ? ( <form onSubmit={handleSubmitReview}> 
+        {getUserId() ? (
+          <div>
             <p>Your Rating</p>
             <Rating rating={rating} onClick={onClick} />
-            <div className="grid gap-4">
+            <form onSubmit={formik.handleSubmit} className="grid gap-4">
               <div className="grid gap-4 grid-cols-2">
-                <input
-                  ref={nameRef}
+                <Input
                   className="rounded-full py-2 px-4 outline-none bg-app-ash-1 w-full col-span-2"
                   placeholder="Name"
+                  formik={formik}
+                  name={"name"}
                 />
-                <input
-                  ref={emailRef}
+                <Input
                   className="rounded-full py-2 px-4 outline-none bg-app-ash-1 w-full col-span-2"
                   placeholder="Email"
+                  formik={formik}
+                  name={"email"}
                 />
               </div>
-              <textarea
-                ref={reviewRef}
-                cols="30"
-                rows="6"
-                placeholder="Your Review"
-                className="rounded-3xl py-4 px-4 outline-none bg-app-ash-1 w-full"
-              ></textarea>
-              <Button className="bg-app-black text-white font-medium w-fit">
+              <div className="py-2 w-full">
+                <textarea
+                  cols="30"
+                  rows="6"
+                  placeholder="Your Review"
+                  className="rounded-3xl py-4 px-4 outline-none bg-app-ash-1 w-full"
+                  {...formik.getFieldProps("review")}
+                ></textarea>
+                {formik.errors["review"] && (
+                  <p className="text-app-red"> {formik.errors["review"]}</p>
+                )}
+              </div>
+              <Button
+                type="submit"
+                className="bg-app-black text-white font-medium w-fit hover:bg-black disabled:bg-[#999999] hover:disabled:bg-[#999999]"
+                disabled={!canSubmit}
+              >
                 Submit Review
               </Button>
-            </div>
-          </form>)
-     :
-     <>
-     <p>You Need To be logged in to Give a Review</p>
-     </>  
-         }
-        </div>
+            </form>
+          </div>
+        ) : (
+          <>
+            <p>You Need To be logged in to Give a Review</p>
+          </>
+        )}
+      </div>
     </div>
   );
 };
@@ -177,4 +191,3 @@ const Rating = ({ rating = 0, onClick = () => {} }) => {
     </div>
   );
 };
-
--- src/hooks/utils/useCanSubmitFormik.js
+++ src/hooks/utils/useCanSubmitFormik.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
-export const useCanSubmitForm = (formik) => {
+export const useCanSubmitForm = (formik, ...rest) => {
   if (!formik) throw new Error("No formik object provided");
   const [canSubmit, setCanSubmit] = useState(false);
   useEffect(() => {
-    setCanSubmit(formik.isValid && formik.dirty);
-  }, [formik]);
+    setCanSubmit(formik.isValid && formik.dirty && rest.every((x) => x));
+  }, [formik, ...rest]);
   return canSubmit;
 };

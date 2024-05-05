f9953aff1f41b4baf28d4fd5fdd443355e7a48bf
Christian <christianchisom278@gmail.com>
2024-05-05 19:43:22 +0100

    update password

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -11,10 +11,13 @@ import { useState } from "react";
 import { PInput } from "../components/Input";
 import { useFormik } from "formik";
 import { useMutation } from "@tanstack/react-query";
+import axios from "../utils/axios";
+import toast from "react-hot-toast";
 
 export const Component = () => {
   const { getUserEmail, username, getUserId } = useAuth();
   const [editMode, setEditMode] = useState(false);
+  const [updatingPassword, setUpdatingPassword] = useState(false);
   const [val, setVal] = useState(username);
   const toggle = () => {
     setEditMode((v) => !v);
@@ -22,21 +25,14 @@ export const Component = () => {
 
   const nameMutation = useMutation({
     mutationFn: (values) => {
-      alert(1)
       return axios.post(`edit/user/${getUserId()}`, values, {
         headers: { "Content-Type": "application/json" },
       });
     },
-    onSuccess: (response) => {
-      console.log(response);
-    },
-    onError: () => {
-      toast.error("Update attempt failed");
-    },
   });
 
   const schema = yup.object().shape({
-    new_password: yup
+    currentPassword: yup
       .string()
       .trim()
       .required("Required field")
@@ -44,22 +40,34 @@ export const Component = () => {
       .matches(/^(?=.*[a-z])/, "Must contain lowercase")
       .min(6, "must be at least 6 characters long")
       .max(50, "must be at most 50 characters long"),
-    // .matches(/(?=.*[^\w\d\s])/, "must contain special character")
-    current_password: yup
+    newPassword: yup
       .string()
       .trim()
       .required("Required field")
       .matches(/(?=.*[A-Z])/, "must contain uppercase")
       .matches(/^(?=.*[a-z])/, "Must contain lowercase")
       .min(6, "must be at least 6 characters long")
-      .max(50, "must be at most 50 characters long"),
-    // .matches(/(?=.*[^\w\d\s])/, "must contain special character")
+      .max(50, "must be at most 50 characters long")
+      .notOneOf(
+        [yup.ref("currentPassword"), null],
+        "Passwords must be different"
+      ),
   });
+
   const formik = useFormik({
-    initialValues: { new_password: "", current_password: "" },
+    initialValues: { newPassword: "", currentPassword: "" },
     validationSchema: schema,
     onSubmit: async (values) => {
       nameMutation.mutate(values);
+      try {
+        setUpdatingPassword(true);
+        await axios.post("user/change/password", values);
+        toast.success(error.response?.data?.message || "Password updated");
+      } catch (error) {
+        toast.error(error.response?.data?.message || "Update attempt failed");
+      } finally {
+        setUpdatingPassword(false);
+      }
     },
   });
   return (
@@ -90,10 +98,10 @@ export const Component = () => {
             </div>
             <div className="pt-6">
               <div>
-                <p className="font-bold">Name:</p> 
+                <p className="font-bold">Name:</p>
                 <div className="flex justify-between gap-6">
-                  ( 
-                    <> 
+                  {editMode ? (
+                    <>
                       <input
                         type="text"
                         value={val}
@@ -101,31 +109,31 @@ export const Component = () => {
                         onChange={(e) => {
                           setVal(e.target.value);
                         }}
-                      /> 
+                      />
                       <button
                         onClick={() => {
                           nameMutation.mutate({ username: val });
                           toggle();
                         }}
-                      > 
-                         <Icon
+                      >
+                        <Icon
                           icon="mingcute:check-2-fill"
                           style={{ fontSize: 32 }}
-                        /> 
-                       </button> 
-                     </> 
-                     ( 
-                     <> 
-                     <p>{username}</p> 
+                        />
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <p>{username}</p>
                       <button onClick={toggle}>
                         <Icon
-                        icon="lucide:edit" 
-                           style={{ fontSize: 32 }}
-                       className="text-app-ash-2"
-                        /> 
-                      </button> 
-                     </>
-                  )
+                          icon="lucide:edit"
+                          style={{ fontSize: 32 }}
+                          className="text-app-ash-2"
+                        />
+                      </button>
+                    </>
+                  )}
                 </div>
               </div>
               <div className="py-4">
@@ -137,7 +145,7 @@ export const Component = () => {
                 <p>123 4567 890 78</p>
               </div>
               <div>
-              <p className="font-bold">Address</p>
+                <p className="font-bold">Address</p>
                 <p>No 7 morgan Estate</p>
               </div>
             </div>
@@ -147,26 +155,38 @@ export const Component = () => {
             <div className="flex items-center gap-3 border-b-2 pb-4">
               <Heading>Reset Passowrd</Heading>
             </div>
+
             <form onSubmit={formik.handleSubmit} className="pt-6">
               <div className="grid gap-6">
                 <PInput
                   placeholder="Enter Current Password"
                   formik={formik}
-                  name="current_password"
+                  name="currentPassword"
                   className="bg-app-ash-1"
                 />
                 <PInput
                   placeholder="Enter New Password"
                   formik={formik}
-                  name="new_password"
+                  name="newPassword"
                   className="bg-app-ash-1"
                 />
               </div>
-              <Button type='submit' className="bg-app-black text-white font-medium mt-6">
-                Reset
+              <Button
+                type="submit"
+                className="bg-app-black text-white font-medium mt-6 w-28 h-10"
+              >
+                {updatingPassword ? (
+                  <Icon
+                    icon="svg-spinners:6-dots-rotate"
+                    style={{ fontSize: 20 }}
+                    className="mx-auto"
+                  />
+                ) : (
+                  "Reset"
+                )}
               </Button>
-            </form> 
-          </div> 
+            </form>
+          </div>
         </div>
       </Wrapper>
     </>

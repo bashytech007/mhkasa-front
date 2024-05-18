43df3122d5deb78ce5e07199d427aee416dd2913
chisomchris <christianezeanyaeche@gmail.com>
2024-05-18 18:46:57 +0100

    fix: add desktop nav, make bestseller scrollable

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -4,7 +4,7 @@ import { ListGrid } from "./ui/ListGrid";
 import { useLoaderData } from "react-router-dom/dist";
 import useLongPress from "../hooks/utils/useLongPress";
 import { Icon } from "@iconify/react";
-import { useRef,useEffect } from "react";
+import { useRef, useEffect } from "react";
 
 export const BestSellers = () => {
   const { bestsellers } = useLoaderData();
@@ -16,11 +16,12 @@ export const BestSellers = () => {
   useEffect(() => {
     setElement(ref.current);
   }, []);
-
   return (
-    <section className="py-8 font-farFetch">
-      <SectionHeader header="Best Sellers" />
-      <div className="flex gap-4">
+    <section className="py-8 hidden md:block">
+      <div className="flex items-center justify-between">
+        <SectionHeader header="Best Sellers" />
+
+        <div className="flex gap-4">
           <button
             {...getHandlers("backward")}
             className="h-10 w-10 bg-white rounded-full grid place-items-center hover:scale-105"
@@ -34,8 +35,9 @@ export const BestSellers = () => {
             <Icon icon="fa6-solid:angle-left" hFlip style={{ fontSize: 28 }} />
           </button>
         </div>
-      <ListGrid>
-      
+      </div>
+
+      <ul className="pt-8 flex gap-6 overflow-auto no-scrollbar" ref={ref}>
         {bestsellers.map(
           (
             { product, category, originalPrice, discountedPrice, image, id },
@@ -49,12 +51,12 @@ export const BestSellers = () => {
                 discountedPrice={discountedPrice}
                 image={image}
                 id={id}
+                className="min-w-52"
               />
             </li>
           )
         )}
-        
-      </ListGrid>
+      </ul>
     </section>
   );
 };

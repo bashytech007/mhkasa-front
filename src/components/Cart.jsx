9d80591b98102b5462fdedfbe8caccd2f2b82d85
chisomchris <christianezeanyaeche@gmail.com>
2024-06-13 23:40:09 +0100

    fix cart item layout

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,14 +3,13 @@ import { CartItem } from "./CartItem";
 
 export const Cart = () => {
   return (
-
     <div className="bg-white py-6 px-4 rounded-3xl my-4 md:px-8">
       <div className="grid items-center font-bold font-FarfetchBold border-b-2 pb-4 grid-cols-12">
         <p className="md:col-span-6">Items</p>
         <div className="hidden md:grid items-center md:col-span-6 grid-cols-1 md:grid-cols-6">
-        <p className="text-center md:col-span-2">Unit Price</p>
-        <p className="text-center md:col-span-2">Qty</p>
-        <p className="text-center md:col-span-2">Total</p>
+          <p className="text-center md:col-span-2">Unit Price</p>
+          <p className="text-center md:col-span-2">Qty</p>
+          <p className="text-center md:col-span-2">Total</p>
         </div>
       </div>
       <CartItems />
@@ -18,7 +17,7 @@ export const Cart = () => {
   );
 };
 
-export const CartItems = () => {
+export const CartItems = ({ isCheckout }) => {
   const { status, data } = useCartQuery();
   return status === "pending" ? (
     "Loading Cart"
@@ -28,7 +27,7 @@ export const CartItems = () => {
     <ul>
       {data.items.map((item, i) => (
         <li key={i} className="py-2">
-          <CartItem item={item} />
+          <CartItem item={item} isCheckout={isCheckout} />
         </li>
       ))}
     </ul>
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -99,12 +99,12 @@
 //   return (
 //     <div className="text-sm border-b-2 sm:text-md">
 //       <div className="grid grid-cols-12 items-center">
-        
+
 //         <div className="flex items-center gap-2 col-span-9 md:col-span-6 md:gap-12">
 //           <div className="w-20 min-h-[96px] overflow-hidden bg-app-ash-1">
 //             <img src={item.productId.mainImage} alt="" className="w-full h-full object-cover" />
 //           </div>
-            
+
 //           <div>
 //             <p className="">{item.productId.name}</p>
 //             <button
@@ -133,7 +133,7 @@
 //           #{item.quantity * item.price}
 //         </p>
 //         </div>
-    
+
 //       </div>
 //       {showModal && (
 //         <Modal title="Remove Item From Cart?">
@@ -256,18 +256,21 @@ import { useState } from "react";
 import { useCartContext } from "../hooks/utils/useCart";
 import toast from "react-hot-toast";
 
-export const CartItem = ({ item }) => {
+export const CartItem = ({ item, isCheckout }) => {
   const [showModal, setShowModal] = useState(false);
   const { removeFromCart } = useCartContext();
   return (
     <div className="text-sm border-b-2 sm:text-md">
       <div className="grid grid-cols-12 items-center">
-        
         <div className="flex items-center gap-2 col-span-9 md:col-span-6 md:gap-12">
-          <div className="w-20 min-h-[96px]  bg-white">
-            <img src={item.productId.mainImage} alt="main-image" className="md:w-40 w-full h-full object-cover" />
+          <div className="w-20 min-h-[96px] bg-white">
+            <img
+              src={item.productId.mainImage}
+              alt="main-image"
+              className="md:w-40 w-full h-full object-cover"
+            />
           </div>
-            
+
           <div>
             <p className="line-clamp-1">{item.productId.name}</p>
             <button
@@ -280,26 +283,39 @@ export const CartItem = ({ item }) => {
             </button>
           </div>
         </div>
-        <div className="col-span-3 sm:col-span-1 items-center md:col-span-6 grid grid-cols-1 md:grid-cols-6 ">
-        <p className="hidden md:block md:text-center md:col-span-2">
-          #{item.price}
-        </p>
+        {isCheckout ? (
+          <div className="col-span-3 sm:col-span-1 items-center md:col-span-6 grid grid-cols-1">
+            <div className="grid items-center justify-end md:justify-center md:items-start md:text-center">
+              <CartItemQuantity
+                quantity={item.quantity}
+                productId={item.productId._id}
+              />
+            </div>
 
-        <div className="grid items-center md:col-span-2 justify-end md:justify-center md:items-start md:text-center">
-          <CartItemQuantity
-            quantity={item.quantity}
-            productId={item.productId._id}
-          />
+            <p className="text-right md:text-center">
+              #{item.quantity * item.price}
+            </p>
+          </div>
+        ) : (
+          <div className="col-span-3 sm:col-span-1 items-center md:col-span-6 grid grid-cols-1 md:grid-cols-6 ">
+            <p className="hidden md:block md:text-center md:col-span-2">
+              #{item.price}
+            </p>
 
+            <div className="grid items-center md:col-span-2 justify-end md:justify-center md:items-start md:text-center">
+              <CartItemQuantity
+                quantity={item.quantity}
+                productId={item.productId._id}
+              />
+            </div>
 
-        </div>
-<p className="text-right md:text-center md:col-span-2">
-          #{item.quantity * item.price}
-        </p>
-       
-        </div>
-    
+            <p className="text-right md:text-center md:col-span-2">
+              #{item.quantity * item.price}
+            </p>
+          </div>
+        )}
       </div>
+
       {showModal && (
         <Modal title="Remove Item From Cart?">
           <p className="text-center p-4">

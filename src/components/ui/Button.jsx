e51531d8fc8ea11a9ef38a0c0ba5d5f89ea1512f
bashytech007 <bashiraremu474@gmail.com>
2024-06-29 09:10:40 +0100

    changes-to-320px

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -11,7 +11,7 @@ const buttonVariants = cva("py-2 w-full font-Helvetica", {
   variants: {
     variant: {
       pill: "rounded-full font-Helvetica",
-      rectangle:"rounded-lg bg-app-black text-center text-white font-Helvetica inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+      rectangle:"rounded-lg bg-app-black text-center text-white font-Helvetica inline-flex items-center justify-center whitespace-nowrap font-normal ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
     },
   },
   defaultVariants: {
